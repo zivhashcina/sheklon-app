d6c474291177f0da48ebc08ca6ccabd92fd9a676
zivhashcina <sismabat@gmail.com>
2025-06-03 22:35:09 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import HomeScreen from "./HomeScreen";
+import RegisterScreen from "./RegisterScreen";
 
 function App() {
-  return <HomeScreen />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/register" element={<RegisterScreen />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
