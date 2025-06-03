b5fcb940a5cfa80660620ef3ec88663244f46492
zivhashcina <sismabat@gmail.com>
2025-06-03 23:37:00 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeScreen from "./HomeScreen";
 import RegisterScreen from "./RegisterScreen";
+import BusinessScreen from "./BusinessScreen";
+import CommunityScreen from "./CommunityScreen";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomeScreen />} />
         <Route path="/register" element={<RegisterScreen />} />
+        <Route path="/business" element={<BusinessScreen />} />
+        <Route path="/community" element={<CommunityScreen />} />
       </Routes>
     </Router>
   );
