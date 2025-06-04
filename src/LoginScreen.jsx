bc9f09b17136b68289a0f365749dc1d4b1db1cf5
zivhashcina <sismabat@gmail.com>
2025-06-04 15:31:08 +0300

    Create LoginScreen.jsx

---
+++ src/LoginScreen.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function LoginScreen() {
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-2xl font-bold mb-4">מסך כניסה</h1>
+      <p>כאן תוכל להיכנס לחשבון שלך.</p>
+    </div>
+  );
+}
